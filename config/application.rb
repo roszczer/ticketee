b55c4b587c6d4dec41b94a5002fff3ca9b40bfaf
Robert Szczerbak <roszczer@gmail.com>
2012-12-21 18:19:32 +0100

    Add validation to ensure names are specified when creating projects

--- config/application.rb
+++ config/application.rb
@@ -23,11 +23,12 @@ module Ticketee
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+    # config.active_record.observers = :cacher,s :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Warsaw'
+    config.active_record.default_timezone = 'Warsaw'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
